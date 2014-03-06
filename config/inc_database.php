87a8a24aed9b90c5364a27d8bf901dc7c2a91055
RobV56 <robv56@gmail.com>
2014-03-05 21:37:31 -0500

    Llama Test Push

---
+++ config/inc_database.php
@@ -0,0 +1,21 @@
+<?php
+
+$path = array(
+	'dev' => 'D:\Copy\wamp\www\_Demo1\pear',
+	'live' => '/home/wfadmin/public_html/pear'
+);
+$database = array(
+	'dev' => array(
+		'user' => 'root',
+		'pass' => '',
+		'host' => 'localhost',
+		'db_name' => 'msg_windsor_main'
+	),
+	'live' => array(
+		'user' => 'wfadmin',
+		'pass' => 'wfacce$$1',
+		'host' => 'localhost',
+		'db_name' => 'wfadmin_main'
+	),
+);
+
